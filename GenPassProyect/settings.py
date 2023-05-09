8328923040a45cfcfbe7476a1f4e990e533f6fe7
Bryan Rodriguez <Serphp@hotmail.com>
2023-05-09 10:06:01 -0500

    [Backend] Connect MySQL to Django

--- GenPassProyect/settings.py
+++ GenPassProyect/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    ##
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
@@ -74,10 +75,21 @@ WSGI_APPLICATION = "GenPassProyect.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db_folder/db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "genpassdb",
+        "USER": "serph",
+        "PASSWORD": "1999",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
